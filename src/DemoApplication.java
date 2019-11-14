4281a123ec29eab526089dee764d6205d4565a1a
cosmopod <fraurerod@gmail.com>
2019-11-15 00:50:45 +0100

    updated hibernate config

--- src/DemoApplication.java
+++ src/DemoApplication.java
@@ -14,7 +14,9 @@ public class DemoApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    //todo    
+
     }
-    
+
 }
